1574a1b77d7f40c5f21500e66ef2e2ed83ad8fd0
Lavisy Randria <mananjolavisy2@gmail.com>
2026-01-24 19:37:53 +0200

    App JS modified

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/contexts/AuthContext';
 import { createTables } from './src/database/migrations';
 import { seedDatabase } from './src/database/seed';
